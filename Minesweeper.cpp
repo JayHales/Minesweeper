d89f4371c963ef46114d6a1d29f05d21d170dc1f
josh <joshhales2004@gmail.com>
2021-05-02 21:55:49 +0100

    a

--- Minesweeper.cpp
+++ Minesweeper.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-
+#include <cstring>
 int MINE = 1;
 int EXPOSED = 2;
 int FLAG = 4;
@@ -10,10 +10,12 @@ int rndint() {
 }
 
 void circle(char(*map)[10][10], int w, int z) {
-    
-    for (int x = (w < 1 ? 0 : -1); x < (w > 8 ? 0 : 2); x++) {
-        for (int y = (z < 1 ? 0 : -1); y < (z > 8 ? 0 : 2); y++) { //|| Line below? TODO
-            if (x + w, y + z) continue;
+    (*map)[w][z] += EXPOSED;
+    for (int x = (w < 1 ? 0 : -1); x <= (w == 9 ? 0 : 1); x++) {
+        for (int y = (z < 1 ? 0 : -1); y <= (z == 9 ? 0 : 1); y++) { //|| Line below? TODO
+	    if ((*map)[x + w][y + z] & EXPOSED) continue;
+	    if ((*map)[x + w][y + z] >> 3 == 0) circle(map, x + w, y + z);
+	    else (*map)[x + w][y + z] += EXPOSED;
             
         }
     }
@@ -21,7 +23,7 @@ void circle(char(*map)[10][10], int w, int z) {
 }
 
 void render(char (*map)[10][10]) {
-    system("cls");
+    system("clear");
     for (int x = 0; x < 10; x++) {
 
         //TODO: Make columsna and rows work
@@ -46,7 +48,7 @@ int main()
     char map[10][10];
     memset(map, 0, sizeof(map));
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 4; i++) {
         int w = rndint();
         int z = rndint();
         if (map[w][z] & MINE) {
@@ -56,8 +58,8 @@ int main()
 
         map[w][z] = MINE;
 
-        for (int x = (w < 1 ? 0 : -1); x < (w > 8 ? 0 : 2); x++) {
-            for (int y = (z < 1 ? 0 : -1); y < (z > 8 ? 0 : 2); y++) { //|| Line below? TODO
+        for (int x = (w < 1 ? 0 : -1); x <= (w == 9 ? 0 : 1); x++) {
+            for (int y = (z < 1 ? 0 : -1); y <= (z == 9 ? 0 : 1); y++) { //|| Line below? TODO
                 if (x == 0 && y == 0) continue;
                 map[x + w][y + z] = map[x + w][y + z] + 8;                
             }
@@ -82,10 +84,12 @@ int main()
             if (map[x][y] & MINE) {
                 std::cout << "GAME OVER!";
                 break;
-            }
+	    }            
 
             if (map[x][y] >> 3 == 0) {
-                // reveal circle
+
+		char(*p)[10][10] = &map;
+                circle(p, x, y);
             }
             else {
                 map[x][y] = map[x][y] + EXPOSED;
